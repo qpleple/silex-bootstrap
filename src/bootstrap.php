d6368299da74f3e5bc2d3c1ad9db6057a9e6c45e
Lucas Mezencio <lucas.mezencio@gmail.com>
2012-08-26 13:20:45 -0300

    Create an additional variable $ini_config to avoid accidentally overwriting it and make the code easier to read

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,8 +7,8 @@ use Silex\Provider\DoctrineServiceProvider;
 use Repository\DumbRepository;
 
 $env = getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production';
-$config = parse_ini_file(__DIR__.'/config.ini', TRUE);
-$config = $config[$env];
+$ini_config = parse_ini_file(__DIR__.'/config.ini', TRUE);
+$config = $ini_config[$env];
 
 $app = new Silex\Application();
 
